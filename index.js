b2413c2724339454c71e1a6a01ec48a2eadc1058
oualidelhouari <elhouarioualid@gmail.com>
2023-08-30 19:50:29 +0100

    assignment completed

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  let arr = [];
+  for (const num of array) {
+    const contain = target - num;
+    if (arr.includes(contain)) {
+      return true;
+    }
+    arr.push(num);
+  }
+  return false;
+
 }
 
-/* 
-  Write the Big O time complexity of your function here
+/*
+ the bog O complexity of time is  O(n²).
 */
 
-/* 
+/*
   Add your pseudocode here
 */
 
 /*
-  Add written explanation of your solution here
+  i will try to assign ampty array than i will tray to 
+   iterate over the array using the for loop  and than i will 
+   subtruct the num from target and register that num 
+   in the ampty array and than use if statement to see in
+    the other iteration if the second arr contain the result 
+    of the new subtruction which is already a registered num 
+    in that arr if true it means  mean the original
+    array has target sum inside it if false that mean the original
+    array has no target sum inside it 
+ 
+ 
+ 
+  
+  
 */
 
 // You can run `node index.js` to view these console logs
